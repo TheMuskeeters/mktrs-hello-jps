069df7b9c9b48c7e9e9001d90e37f8f1c403689a
Carlos Adolfo Ortiz Quiros <carlosaortiz@yahoo.com>
2024-03-20 21:34:07 -0500

    Add initial files.
    The files being commited were worked days before this commit.
    It started on Mar.09/2024

---
+++ src/test/java/com/themusketeers/jps/ApplicationTests.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Source File:   APPLICATIONTESTS.JAVA                                       */
+/* Copyright (c), 2024 The Musketeers                                         */
+/*----------------------------------------------------------------------------*/
+/*-----------------------------------------------------------------------------
+ History
+ Mar.09/2024  COQ  File created.
+ -----------------------------------------------------------------------------*/
+package com.themusketeers.jps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
